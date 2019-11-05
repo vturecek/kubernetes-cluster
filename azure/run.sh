0618d5bbc1719b1fef4babd38850e90540a8f326
Vaclav Turecek <vturecek@microsoft.com>
2019-11-04 17:13:43 -0800

    Adding kubeconfig and etcd setup

--- azure/run.sh
+++ azure/run.sh
@@ -90,8 +90,89 @@ cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=./pki/ca-config.json -hostna
 # Service accounts
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=./pki/ca-config.json -profile=kubernetes ./pki/service-account-csr.json | cfssljson -bare service-account
 
+# Connect to VPN before running this - using internal VM IP
 for i in 0 1 2; do
-    INTERNALIP=$(az vm show -d -n worker-${i} -g $RESOURCEGROUP --query privateIps -otsv)
-    scp -o StrictHostKeyChecking=no ca.pem worker-${i}-key.pem worker-${i}.pem kuberoot@${INTERNALIP}:~/
+    WORKER_INTERNALIP=$(az vm show -d -n worker-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no ca.pem worker-${i}-key.pem worker-${i}.pem kuberoot@${WORKER_INTERNALIP}:~/
 done
-scp -o StrictHostKeyChecking=no ca.pem worker-0-key.pem worker-0.pem kuberoot@
+
+for i in 0 1 2; do
+    CONTROLLER_INTERNALIP=$(az vm show -d -n controller-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem service-account-key.pem service-account.pem kuberoot@${CONTROLLER_INTERNALIP}:~/
+done
+
+# Kubernetes config files
+CLUSTER_IP=$(az network public-ip show -g $RESOURCEGROUP -n $PUBLICIP --query ipAddress -otsv)
+
+# Kubelets for worker nodes
+for i in 0 1 2; do
+    kubectl config set-cluster vturecek-kube --certificate-authority=ca.pem --embed-certs=true --server=https://${CLUSTER_IP}:6443 --kubeconfig=worker-${i}.kubeconfig
+    kubectl config set-credentials system:node:worker-${i} --client-certificate=worker-${i}.pem --client-key=worker-${i}-key.pem --embed-certs=true --kubeconfig=worker-${i}.kubeconfig
+    kubectl config set-context default --cluster=vturecek-kube --user=system:node:worker-${i} --kubeconfig=worker-${i}.kubeconfig
+    kubectl config use-context default --kubeconfig=worker-${i}.kubeconfig
+done
+
+# Kubeproxy for worker nodes
+kubectl config set-cluster vturecek-kube --certificate-authority=ca.pem --embed-certs=true --server=https://${CLUSTER_IP}:6443 --kubeconfig=kube-proxy.kubeconfig
+kubectl config set-credentials kube-proxy --client-certificate=kube-proxy.pem --client-key=kube-proxy-key.pem --embed-certs=true --kubeconfig=kube-proxy.kubeconfig
+kubectl config set-context default --cluster=vturecek-kube --user=kube-proxy --kubeconfig=kube-proxy.kubeconfig
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+
+# Controller manager for controller nodes
+kubectl config set-cluster vturecek-kube --certificate-authority=ca.pem --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=kube-controller-manager.kubeconfig
+kubectl config set-credentials system:kube-controller-manager --client-certificate=kube-controller-manager.pem --client-key=kube-controller-manager-key.pem --embed-certs=true --kubeconfig=kube-controller-manager.kubeconfig
+kubectl config set-context default --cluster=vturecek-kube --user=system:kube-controller-manager --kubeconfig=kube-controller-manager.kubeconfig
+kubectl config use-context default --kubeconfig=kube-controller-manager.kubeconfig
+
+# Scheduler for controller nodes
+kubectl config set-cluster vturecek-kube --certificate-authority=ca.pem --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=kube-scheduler.kubeconfig
+kubectl config set-credentials system:kube-scheduler --client-certificate=kube-scheduler.pem --client-key=kube-scheduler-key.pem --embed-certs=true --kubeconfig=kube-scheduler.kubeconfig
+kubectl config set-context default --cluster=vturecek-kube --user=system:kube-scheduler --kubeconfig=kube-scheduler.kubeconfig
+kubectl config use-context default --kubeconfig=kube-scheduler.kubeconfig
+
+# Admin for controller nodes
+kubectl config set-cluster vturecek-kube --certificate-authority=ca.pem --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=admin.kubeconfig
+kubectl config set-credentials admin --client-certificate=admin.pem --client-key=admin-key.pem --embed-certs=true --kubeconfig=admin.kubeconfig
+kubectl config set-context default --cluster=vturecek-kube --user=admin --kubeconfig=admin.kubeconfig
+kubectl config use-context default --kubeconfig=admin.kubeconfig
+
+# Connect to VPN before running this - using internal VM IP
+for i in 0 1 2; do
+    WORKER_INTERNALIP=$(az vm show -d -n worker-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no worker-${i}.kubeconfig kube-proxy.kubeconfig kuberoot@${WORKER_INTERNALIP}:~/
+done
+
+for i in 0 1 2; do
+    CONTROLLER_INTERNALIP=$(az vm show -d -n controller-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig kuberoot@${CONTROLLER_INTERNALIP}:~/
+done
+
+# data encryption 
+# get a key
+ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+cat > encryption-config.yaml <<EOF
+kind: EncryptionConfig
+apiVersion: v1
+resources:
+  - resources:
+      - secrets
+    providers:
+      - aescbc:
+          keys:
+            - name: key1
+              secret: ${ENCRYPTION_KEY}
+      - identity: {}
+EOF
+
+
+for i in 0 1 2; do
+    CONTROLLER_INTERNALIP=$(az vm show -d -n controller-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no encryption-config.yaml kuberoot@${CONTROLLER_INTERNALIP}:~/
+done
+
+# Run etcd set up script on each controller
+for i in 0 1 2; do
+    CONTROLLER_INTERNALIP=$(az vm show -d -n controller-${i} -g $RESOURCEGROUP --query privateIps -otsv)
+    scp -o StrictHostKeyChecking=no ./azure/setup-etcd.sh kuberoot@${CONTROLLER_INTERNALIP}:~/
+    ssh -f kuberoot@${CONTROLLER_INTERNALIP} "nohup ./setup-etcd.sh > /dev/null 2>&1"
+done
